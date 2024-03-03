fbe2f8a3f7e74c97e1f2a1dcee92d5e93a2ac676
Matias San Martin <sanmartinsantandermatiasaaron@gmail.com>
2024-03-03 11:05:04 -0300

    done

--- index.js
+++ index.js
@@ -2,3 +2,6 @@ precio = 400000
 
 precioSpan = document.querySelector(".precio-inicial");
 precioSpan.innerHTML = precio
+
+const cantidadComprar = document.querySelector(".cantidad")
+const valorTotal = document.querySelector(".valor-total")
